be494ecf9136418e59564ee58743b63e0f65e187
ulhasrm <ulhas@ulhas-mbp.efi.internal>
2020-03-20 14:51:30 +0530

    distance calculation changes

--- src/main/java/org/springframework/data/hazelcast/repository/query/GeoPredicate.java
+++ src/main/java/org/springframework/data/hazelcast/repository/query/GeoPredicate.java
@@ -29,8 +29,8 @@ import com.hazelcast.query.impl.Extractable;
 /**
  * Geo Predicate - Used to calculate near and within queries
  * <li>Finds all the Points within the given distance range from source Point.
- * <li>Finds all the Points within given Circle.  
- * 
+ * <li>Finds all the Points within given Circle.
+ *
  * @param <K> key of map entry
  * @param <V> value of map entry
  * @author Ulhas R Manekar
@@ -39,10 +39,9 @@ public class GeoPredicate<K, V>
         implements Predicate<K, V> {
 
     private static final long serialVersionUID = 1L;
-    private static final double MILES_TO_KM = 1.609344;
-    private static final double MILES_TO_NEUTRAL = 0.8684;
-    private static final double MINUTES_IN_DEGREE = 60;
-    private static final double STATUTE_MILES_IN_NAUTICAL_MILE = 1.1515;
+    private static final double KM_TO_MILES = 0.621371;
+    private static final double KM_TO_NEUTRAL = 0.539957;
+    private static final double R = 6372.8;
 
     final String attributeName;
     final Point queryPoint;
@@ -53,11 +52,11 @@ public class GeoPredicate<K, V>
      * Constructs a new geo predicate on the given point
      *
      * @param attribute    the name of the attribute in a object within Map which is of type Point.
-     * 
+     *
      * @param point        the source point from where the distance is calculated.
-     * 
+     *
      * @param distance     the Distance object with value and unit of distance.
-     * 
+     *
      */
     public GeoPredicate(String attribute, Point point, Distance distance) {
         this.attributeName = canonicalizeAttribute(attribute);
@@ -83,22 +82,38 @@ public class GeoPredicate<K, V>
         return withinRange;
     }
 
+    /**
+     * This method users Haversine formula to calculate the distance between two points
+     * Formula is explained here - https://www.movable-type.co.uk/scripts/gis-faq-5.1.html
+     * Sample Java code is here - https://rosettacode.org/wiki/Haversine_formula#Java
+     * @param lat1 - Latitude of first point.
+     * @param lng1 - Longitude of first point.
+     * @param lat2 - Latitude of second point.
+     * @param lng2 - Longitude of second point.
+     * @param metric - metric to specify where its KILOMETERS, MILES or NEUTRAL
+     * @return
+     */
     private double calculateDistance(double lat1, double lng1, double lat2, double lng2, Metric metric) {
         if ((lat1 == lat2) && (lng1 == lng2)) {
             return 0;
         } else {
-            double theta = lng1 - lng2;
-            double dist = Math.sin(Math.toRadians(lat1)) * Math.sin(Math.toRadians(lat2)) + Math.cos(Math.toRadians(lat1))
-            * Math.cos(Math.toRadians(lat2)) * Math.cos(Math.toRadians(theta));
-            dist = Math.acos(dist);
-            dist = Math.toDegrees(dist);
-            dist = dist * MINUTES_IN_DEGREE * STATUTE_MILES_IN_NAUTICAL_MILE;
-            if (Metrics.KILOMETERS.equals(metric)) {
-                dist = dist * MILES_TO_KM;
+            double dLat = Math.toRadians(lat2 - lat1);
+            double dLon = Math.toRadians(lng2 - lng1);
+            lat1 = Math.toRadians(lat1);
+            lat2 = Math.toRadians(lat2);
+
+            double a = Math.pow(Math.sin(dLat / 2), 2)
+                + Math.pow(Math.sin(dLon / 2), 2) * Math.cos(lat1) * Math.cos(lat2);
+            double c = 2 * Math.asin(Math.sqrt(a));
+            double dist = R * c;
+
+            if (Metrics.MILES.equals(metric)) {
+                dist = dist * KM_TO_MILES;
             } else if (Metrics.NEUTRAL.equals(metric)) {
-                dist = dist * MILES_TO_NEUTRAL;
+                dist = dist * KM_TO_NEUTRAL;
             }
-            return (dist);
+
+            return dist;
         }
     }
 
